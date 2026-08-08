580458348f332fc4b73bca00fe56e6c325840258
Ilham maulana syahid <ilhammaulanasyahid@students.undip.ac.id>
2026-08-09 06:09:53 +0700

    Remove duplicate transaction entry in config.js

--- config.js
+++ config.js
@@ -4,6 +4,4 @@
 //   Lokal:        http://localhost:3000/?id=NO_REKENING
 //   GitHub Pages: https://USERNAME.github.io/NAMA-REPO/?id=NO_REKENING
 const DATA_TRANSAKSI = [
-  { id: '562312345678', nama: 'Budi Santoso',  rekening: '5623 1234 5678', nominal: 'Rp 1.500.000' },
-  { id: '562398765432', nama: 'Siti Aminah',   rekening: '5623 9876 5432', nominal: 'Rp 250.000' }
-];
+  { id: '562398765432', nama: 'Siti Aminah', rekening: '5623 9876 5432', nominal: 'Rp 250.000' },];
